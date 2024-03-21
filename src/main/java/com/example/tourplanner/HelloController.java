8eddabca6528600a50e828d41895d5db9225ddb3
JohannesHenz <johenz@protonmail.com>
2024-03-22 00:14:23 +0100

    added GPT code to fxml

--- src/main/java/com/example/tourplanner/HelloController.java
+++ src/main/java/com/example/tourplanner/HelloController.java
@@ -1,11 +1,23 @@
 package com.example.tourplanner;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private ListView<String> tourList;
+
+    @FXML
+    public void initialize() {
+        ObservableList<String> tours = FXCollections.observableArrayList("Wienerwald", "Dopplerhütte", "Figlwarté", "Dorfrunde");
+        tourList.setItems(tours);
+    }
 
     @FXML
     protected void onHelloButtonClick() {
